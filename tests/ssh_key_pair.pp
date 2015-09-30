2b359a29c31a296ff6d7f491a29008c65b23dac1
Tyler Yahn <tyler@moonshadowmobile.com>
2015-09-30 13:39:57 -0700

    Update smoke test puppet syntax.
    Fix syntax and lint issues with account and ssh_key_pair smoke tests.

--- tests/ssh_key_pair.pp
+++ tests/ssh_key_pair.pp
@@ -3,10 +3,10 @@ user { 'bob': ensure => present, }
 file { '/home/bob': ensure => directory, }
 
 users::ssh_key_pair { 'test-dsa':
+  ensure          => present,
   user            => 'bob',
   home            => '/home/bob',
   key_name        => 'demo-dsa',
-  ensure          => present,
   public_content  => 'Public_key_content',
   private_content => 'Private_key_content',
 }
