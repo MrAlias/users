29def9deb7826c4a6d2f88d903ef764b7e4e3c14
Tyler Yahn <tyler@moonshadowmobile.com>
2015-10-08 12:23:50 -0700

    Update spec_helper_acceptance
    Remove redundant install of puppetlabs/stdlib.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -22,12 +22,8 @@ RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
 
-  # Configure all nodes in nodeset
+  # Ensure this module is installed
   c.before :suite do
     puppet_module_install(:source => module_root, :module_name => module_name)
-
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs/stdlib'), { :acceptable_exit_codes => [0] }
-    end
   end
 end
